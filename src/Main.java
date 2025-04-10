685df8b6d6cf9424d2af66b926f972716b28af54
janva111 <vavraj08@gmail.com>
2025-04-10 11:23:41 +0200

    Create

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        /*
+                          🏰 Kingdom Tycoon – Popis hry
+
+             Kingdom Tycoon je grafická klikací tycoon hra, ve které se hráč ujímá role
+             vládce vlastního království. Cílem hry je postupně budovat a rozvíjet
+             království pomocí investic, vylepšení a plnění úkolů.
+
+             Hráč začíná s malým množstvím prostředků a získává zlato buď aktivním klikáním, nebo prostřednictvím
+             pasivního příjmu z vybudovaných budov. Každá budova má své vizuální ztvárnění, které se mění podle
+             její úrovně a fáze vývoje.
+
+             Součástí hry jsou i speciální úkoly, za jejichž splnění hráč získá odměny ve formě zlata nebo
+             dočasných boostů. V pozdější fázi hry se zpřístupní prestige systém, který umožní hráči resetovat
+             svůj postup výměnou za speciální měnu. Ta poskytuje trvalé vylepšení příjmů a další výhody.
+         */
+
+
+
+
+    }
+}
